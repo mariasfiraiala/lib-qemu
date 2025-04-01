d9a83b6cc4b195246be1fae1e68081c555d79f29
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2025-04-01 17:04:41 +0300

    include/glib-compat.h: Start adding glib macros
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

---
+++ include/glib-compat.h
@@ -0,0 +1,7 @@
+#ifndef QEMU_GLIB_COMPAT_H
+#define QEMU_GLIB_COMPAT_H
+
+#define G_NORETURN __attribute__ ((__noreturn__))
+#define G_GNUC_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
+
+#endif
